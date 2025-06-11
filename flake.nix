aa121a4883a861992251acb779f7ea2bef8c7f61
reassDev <reassDev@proton.me>
2025-06-11 21:06:58 +0200

    Added a flake.nix because I'm about to learn how to set that up, especially with other hosts and such

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My NixOS Configuration";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.reassOS = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
+    };
+  };
+}
